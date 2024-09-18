0179da8b59bebb1eef996a387d96d97ecacc097b
MartiresValdez17 <61474696+Sonny217@users.noreply.github.com>
2024-09-17 20:43:30 -0400

    Add files via upload

---
+++ massCalc.py
@@ -0,0 +1,17 @@
+#"""Como calcular la masa de un objeto"""
+#"""Si sabes el peso de algo que se mide en newtons,
+#puesde usar esta formula"""
+#"""Masa=peso/9.8"""
+#"""How to calculate the mass of an object"""
+#If you know the weight of something that is measured in newtons,
+#you can use this formula”.
+#"""Mass=weight/9.8"""
+
+weight =float(input("Enter weight:"))
+mass = weight/9.8
+print(f"The mass of the object is:{mass}KG")
+
+
+peso = float(input("Ingrese el peso en newtom:"))
+masa = peso/9.8
+print(f"La masa del objeto es de :{masa}KG")
